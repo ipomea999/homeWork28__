37c5623354b542895020187ead4102106945be54
Kutman <ipomea875@gmail.com>
2026-05-12 05:49:11 +0600

    implement filtering using lambdas

---
+++ src/task1/Cat.java
@@ -0,0 +1,61 @@
+package task1;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+final class Cat {
+    private static final Random r = new Random();
+    private static final List<String> names = List.of("Peach", "Ginger", "Toby", "Seth", "Tibbles", "Tabby", "Poppy",
+            "Millie", "Daisy", "Jasper", "Misty", "Minka");
+    private final String name;
+    private final int age;
+    private final Breed breed;
+    private final Color color;
+
+    private Cat() {
+        name = names.get(r.nextInt(names.size()));
+        age = r.nextInt(10) + 1;
+        breed = Breed.values()[r.nextInt(Breed.values().length)];
+        color = Color.values()[r.nextInt(Color.values().length)];
+    }
+
+    public static final List<Cat> makeCats(int amount) {
+        return Stream.generate(Cat::new)
+                .limit(amount)
+                .collect(Collectors.toList());
+    }
+
+    public final Color getColor() {
+        return color;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public final int getAge() {
+        return age;
+    }
+
+    public final Breed getBreed() {
+        return breed;
+    }
+
+    enum Color {
+        TABBY, SILVER, GRAY, PEACH, GINGER, TORTIE
+    }
+
+    enum Breed {
+        SPHINX, BRITAIN, SCOTTISH, VULGARIS, ORIENTAL
+    }
+
+    /*********/
+
+    public boolean isColorGray() {
+        return this.color == Color.GRAY;
+    }
+
+    /*********/
+}
