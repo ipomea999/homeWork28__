265e90a201949f2699df7e2da42c631aa207ddbb
Kutman <ipomea875@gmail.com>
2026-05-12 05:50:28 +0600

    update ActiveCat to use functional interface

---
+++ src/task2/ActiveCat.java
@@ -0,0 +1,40 @@
+package task2;
+
+import java.util.List;
+import java.util.Random;
+
+public final class ActiveCat {
+    private static final Random r = new Random();
+    private static final List<String> names = List.of("Peach", "Ginger", "Toby", "Seth", "Tibbles", "Tabby", "Poppy",
+            "Millie", "Daisy", "Jasper", "Misty", "Minka");
+    private final String name;
+
+    private Action action;
+
+    public ActiveCat(Action action) {
+        name = names.get(r.nextInt(names.size()));
+        this.action = action;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public void doAction() {
+        System.out.printf("Я %s. %s%n", name, action.perform());
+    }
+
+    /*****/
+    public static String jump() {
+        return "Я прыгаю!";
+    }
+
+    public static String sleep() {
+        return "Я сплю!";
+    }
+
+    public static String eat() {
+        return "Я кушаю!";
+    }
+    /*****/
+}
